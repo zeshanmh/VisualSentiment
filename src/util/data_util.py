076d2c2cc52a36726b64123d5dcb62ee24175cfd
Zeshan Hussain <zeshanmh@DN0a22a323.SUNet>
2016-05-12 16:36:58 -0700

    deleted things and added filenames script

--- src/util/data_util.py
+++ src/util/data_util.py
@@ -9,7 +9,13 @@ def get_label_matrix(labels=['Interaction', 'Focus', 'Happiness']):
 	label_matrix = np.vstack((pand_arr['Interaction'].as_matrix(), \
 		pand_arr['Focus'].as_matrix(), pand_arr['Happiness'].as_matrix()))
 	return label_matrix.T
+	
 
+def get_filename_list(): 
+	filenames = open('../../data/groupdataset_release/file_names.txt', 'r')
+	flist = filenames.readlines()
+	return flist[0].split('\r')
+	
 
 if __name__ == '__main__':
-    get_label_matrix()
+    get_filename_list()

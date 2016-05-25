5839561d1b0f8a5980aab2b5b26dc28fc66f716d
Hardie Cate <hardiecate@DN0a226633.SUNet>
2016-05-25 14:24:53 -0700

    added mean/sd calculations of derivatives

--- src/util/EmotionExtractor.py
+++ src/util/EmotionExtractor.py
@@ -38,24 +38,47 @@ class EmotionExtractor:
 					deriv_vec[1] = gray_img_resize[i+1,k] - gray_img_resize[i-1,k]
 					deriv_vec[3] = gray_img_resize[i+1,k] - 2 * gray_img_resize[i,k] + gray_img_resize[i-1,k]
 				else: 
-					deriv_vec[1] = -1
-					deriv_vec[3] = -1
+					deriv_vec[1] = None
+					deriv_vec[3] = None
 
 				if k != 0 and k != NORMALIZED_SIZE - 1: 
 					deriv_vec[0] = gray_img_resize[i,k+1] - gray_img_resize[i,k-1]
 					deriv_vec[2] = gray_img_resize[i,k+1] - 2 * gray_img_resize[i,k] + gray_img_resize[i,k-1]					
 				else: 
-					deriv_vec[0] = -1 
-					deriv_vec[2] = -1
+					deriv_vec[0] = None
+					deriv_vec[2] = None
 
 				if i != 0 and i != NORMALIZED_SIZE - 1 and k != 0 \
 					and k != NORMALIZED_SIZE - 1: 
 					deriv_vec[4] = gray_img_resize[i+1,k+1] - gray_img_resize[i-1,k+1] - gray_img_resize[i+1,k-1] + gray_img_resize[i-1,k-1]
 				else: 
-					deriv_vec[4] = -1
+					deriv_vec[4] = None
 
 				pixel_derivs[i,k] = deriv_vec
+				
+		feature_vec = calc_mean_sd(pixel_derivs)
 
 
-		print pixel_derivs[30,30,:]
+
+	def calc_mean_sd(pixel_derivs):
+		n_cells = NORMALIZED_SIZE / 4
+		n_stats = 5
+		all_stats = np.zeros(n_cells*n_cells*2*n_stats)
+		counter = 0
+		for i in xrange(0, NORMALIZED_SIZE, 4):
+			for j in xrange(0, NORMALIZED_SIZE, 4):
+
+				flat_window = np.reshape(pixel_derivs[i:i+4,j:j+4], (-1, 5))
+				for col in xrange(flat_window.shape[1]):
+					col_list = list(flat_window[:,col])
+					good_list = [x for x in col_list if x is not None]
+					all_stats[counter:counter+n_stats] = np.mean(np.array(good_list))
+					all_stats[counter+n_stats:counter+2*n_stats] = np.std(np.array(good_list))
+					counter += 2*n_stats
+		print all_stats[:10]
+		print all_stats[-10:]
+		return all_stats
+
+
+		# print pixel_derivs[30,30,:]
 
